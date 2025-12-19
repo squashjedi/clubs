ab61492146e0313a34d22b3baad42f2610bb9be2
squashjedi <paulandrewlord@gmail.com>
2025-12-19 16:11:19 +0000

    Test modal glitch

--- resources/views/components/settings/delete-user-form.blade.php
+++ resources/views/components/settings/delete-user-form.blade.php
@@ -28,31 +28,6 @@ new class extends Component {
         <flux:subheading>{{ __('Delete your account and all of its resources') }}</flux:subheading>
     </div>
 
-
-    <flux:modal.trigger name="edit-profile">
-        <flux:button>Edit profile</flux:button>
-    </flux:modal.trigger>
-
-    <flux:modal name="edit-profile" class="md:w-96">
-        <div class="space-y-6">
-            <div>
-                <flux:heading size="lg">Update profile</flux:heading>
-                <flux:text class="mt-2">Make changes to your personal details.</flux:text>
-            </div>
-
-            <flux:input label="Name" placeholder="Your name" />
-
-            <flux:input label="Date of birth" type="date" />
-
-            <div class="flex">
-                <flux:spacer />
-
-                <flux:button type="submit" variant="primary">Save changes</flux:button>
-            </div>
-        </div>
-    </flux:modal>
-
-
     <flux:modal.trigger name="confirm-user-deletion">
         <flux:button variant="danger" x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">
             {{ __('Delete account') }}
