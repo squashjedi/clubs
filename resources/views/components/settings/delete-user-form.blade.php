a84b2159acc40e511359913ea8d958148f732267
squashjedi <paulandrewlord@gmail.com>
2025-12-19 15:59:56 +0000

    Test modal glitch

--- resources/views/components/settings/delete-user-form.blade.php
+++ resources/views/components/settings/delete-user-form.blade.php
@@ -34,7 +34,7 @@ new class extends Component {
         </flux:button>
     </flux:modal.trigger>
 
-    <flux:modal name="confirm-user-deletion" :show="$errors->isNotEmpty()" focusable class="max-w-lg">
+    <flux:modal name="confirm-user-deletion" :show="$errors->isNotEmpty()" focusable class="w-40">
         <form wire:submit="deleteUser" class="space-y-6">
             <div>
                 <flux:heading size="lg">{{ __('Are you sure you want to delete your account?') }}</flux:heading>
