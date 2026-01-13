b6e430f70bb9f445362411e5e155b008149c6bdd
squashjedi <paulandrewlord@gmail.com>
2026-01-13 10:23:41 +0000

    Loading spinner in results matrix

--- resources/views/components/generic/⚡matrix.blade.php
+++ resources/views/components/generic/⚡matrix.blade.php
@@ -718,8 +718,8 @@ new class extends Component
                                     {{-- Per-cell loading overlay while openEdit is running --}}
                                     <div
                                         wire:loading
-                                        wire:target="openEdit({{ $row->id }}, {{ $col->id }})"
-                                        class="absolute inset-0 z-20 bg-white/60 grid place-items-center"
+                                        wire:target="openEdit"
+                                        class="absolute inset-0 z-20 bg-white/60 grid place-items-center pointer-events-none"
                                     >
                                         <div class="flex items-center justify-center h-full">
                                             <flux:icon.loading class="size-6" />
