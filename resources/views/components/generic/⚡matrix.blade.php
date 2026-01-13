669505ab20d2df934fe59a46c4a0c0d6ff46d48b
squashjedi <paulandrewlord@gmail.com>
2026-01-13 13:57:10 +0000

    Loading spinner in results matrix

--- resources/views/components/generic/⚡matrix.blade.php
+++ resources/views/components/generic/⚡matrix.blade.php
@@ -715,17 +715,6 @@ new class extends Component
                                 ])
                             >
                                 @if ($row->id !== $col->id)
-                                    {{-- Per-cell loading overlay while openEdit is running --}}
-                                    <div
-                                        wire:loading
-                                        wire:target="openEdit({{ $row->id }}, {{ $col->id }})"
-                                        class="absolute inset-0 z-20 bg-white/60 grid place-items-center pointer-events-none"
-                                    >
-                                        <div class="flex items-center justify-center h-full">
-                                            <flux:icon.loading class="size-6" />
-                                        </div>
-                                    </div>
-
                                     <div class="absolute inset-0 flex items-center justify-center">
                                         <div
                                             @class([
@@ -773,7 +762,6 @@ new class extends Component
                                             @if (! ($matrix[$row->id][$col->id]['original_for'] || $matrix[$col->id][$row->id]['original_for']))
                                                 <div class="flex items-center justify-center size-full">
                                                     <flux:icon.plus-circle
-                                                        wire:loading.class="hidden"
                                                         wire:target="openEdit({{ $row->id }}, {{ $col->id }})"
                                                         variant="solid"
                                                         class="size-8 text-blue-300"
@@ -782,7 +770,6 @@ new class extends Component
                                             @else
                                                 <div class="absolute top-0.5 right-0.5">
                                                     <flux:icon.pencil-square
-                                                        wire:loading.class="hidden"
                                                         wire:target="openEdit({{ $row->id }}, {{ $col->id }})"
                                                         variant="outline"
                                                         class="size-5 text-blue-300"
