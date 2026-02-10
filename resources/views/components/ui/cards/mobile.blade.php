3f25e8beb8868c3e016bb0385e1b19454cd504d0
squashjedi <paulandrewlord@gmail.com>
2026-02-10 07:34:26 +0000

    Club admin club select

--- resources/views/components/ui/cards/mobile.blade.php
+++ resources/views/components/ui/cards/mobile.blade.php
@@ -1,3 +1,3 @@
-<flux:card {{ $attributes->merge(['class' => 'max-sm:-mx-6 max-sm:border-l-0 max-sm:border-r-0 max-sm:rounded-none space-y-6']) }}>
+<flux:card {{ $attributes->merge(['class' => 'max-sm:-mx-4 max-sm:border-l-0 max-sm:border-r-0 max-sm:rounded-none space-y-6']) }}>
     {{ $slot }}
 </flux:card>
--- resources/views/layouts/club-admin.blade.php
+++ resources/views/layouts/club-admin.blade.php
@@ -6,9 +6,9 @@
 
                 <div tabindex="0" class="fixed inset-0 flex focus:outline-none">
                     <el-dialog-panel
-                        class="group/dialog-panel relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-closed:-translate-x-full"
+                        class="group/dialog-panel relative flex w-full max-w-xs flex-1 pr-16 transform transition duration-300 ease-in-out data-closed:-translate-x-full"
                     >
-                        <div class="absolute top-0 left-full flex w-16 justify-center pt-5 duration-300 ease-in-out group-data-closed/dialog-panel:opacity-0">
+                        <div class="absolute top-0 right-0 flex w-16 justify-center pt-5 duration-300 ease-in-out group-data-closed/dialog-panel:opacity-0">
                             <button type="button" command="close" commandfor="sidebar" class="-m-2.5 p-2.5">
                                 <span class="sr-only">Close sidebar</span>
                                 <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 text-white">
