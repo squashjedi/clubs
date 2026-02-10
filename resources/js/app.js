21ae8c37115c7d865c5da4863206f05f29df9e51
squashjedi <paulandrewlord@gmail.com>
2026-02-10 10:28:47 +0000

    Club admin club select

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,12 +5,14 @@ if ('scrollRestoration' in history) {
 }
 
 const clearNavigateScrollState = () => {
-	document.body.setAttribute('data-scroll-x', '0')
-	document.body.setAttribute('data-scroll-y', '0')
+	queueMicrotask(() => {
+		document.body.removeAttribute('data-scroll-x')
+		document.body.removeAttribute('data-scroll-y')
 
-	document.querySelectorAll('[x-navigate\\:scroll], [wire\\:navigate\\:scroll]').forEach((el) => {
-		el.setAttribute('data-scroll-x', '0')
-		el.setAttribute('data-scroll-y', '0')
+		document.querySelectorAll('[x-navigate\\:scroll], [wire\\:navigate\\:scroll]').forEach((el) => {
+			el.removeAttribute('data-scroll-x')
+			el.removeAttribute('data-scroll-y')
+		})
 	})
 }
 
@@ -20,5 +22,6 @@ const forceScrollTop = () => {
 	document.body.scrollTop = 0
 }
 
+document.addEventListener('alpine:navigating', clearNavigateScrollState)
 document.addEventListener('livewire:navigating', clearNavigateScrollState)
 document.addEventListener('livewire:navigated', forceScrollTop)
