93f1bbc4e07a1cb5f95dca12f43a01f403549bd1
squashjedi <paulandrewlord@gmail.com>
2026-02-10 10:48:28 +0000

    Club admin club select

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,22 @@
         @fluxAppearance
     </head>
     <body class="min-h-dvh">
-        {{ $slot }}
+        <div
+            x-data="{ navigating: false }"
+            x-on:alpine:navigating.window="navigating = true"
+            x-on:livewire:navigating.window="navigating = true"
+            x-on:alpine:navigated.window="navigating = false"
+            x-on:livewire:navigated.window="navigating = false"
+        >
+            <div
+                class="fixed inset-0 z-50 bg-white/70 backdrop-blur-sm"
+                x-show="navigating"
+                x-transition.opacity
+                x-cloak
+            ></div>
+
+            {{ $slot }}
+        </div>
 
         @persist("toast")
             <flux:toast.group position="top end">
